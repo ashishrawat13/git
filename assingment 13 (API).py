948582fd97e6bcc2837d20b2cf8a8df098c194e7
ashishrawat13 <32287172+ashishrawat13@users.noreply.github.com>
2018-09-05 10:50:45 +0530

    13th

---
+++ assingment 13 (API).py
@@ -0,0 +1,8 @@
+import requests
+import json
+parameters={'method':'getQuote','format':'json','lang':"en",'key':''}
+response=requests.get("https://api.forismatic.com/api/1.0/getQuote.json",params=parameters)
+res=response.json()
+print("Random Quote: {} \n                    - {}".format(res['quoteText'],res['quoteAuthor']))
+
+
